d0e20ef2c7fe816b25b28adc3202ba300c62685f
Dmitry Yaroshevsky <89291924+dimonyar@users.noreply.github.com>
2022-04-06 18:07:03 +0300

    new_context filter for user_id

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,6 +1,8 @@
 from accounts.forms import DeviceForm, SignUpForm
 from accounts.models import Device, User
 
+from crum import get_current_user
+
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
@@ -49,9 +51,18 @@ class Devices(ListView):
     template_name = 'devices.html'
     context_object_name = 'devices_list'
 
+    def get_queryset(self):
+        current_id = get_current_user().id
+        new_context = Device.objects.filter(
+            user_id=current_id)
+        return new_context
+
 
 class DeviceCreate(CreateView):
     model = Device
     template_name = 'device_create.html'
     form_class = DeviceForm
     success_url = reverse_lazy('accounts:devices')
+
+    def test_func(self):
+        return self.request.user.is_superuser
